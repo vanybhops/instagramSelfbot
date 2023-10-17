c2eb7276e2bcf38278b73c718379eb5ac52fd71c
vany <79766479+vanybhops@users.noreply.github.com>
2023-10-17 20:30:31 +0200

    Delete utils/password_manager directory

--- utils/password_manager/format.js
+++
@@ -1,6 +0,0 @@
-function format(version, timestamp, password, username){
-    return `enc_password=`+
-            encodeURIComponent(`#PWD_INSTAGRAM_BROWSER:${version}:${timestamp}:${password}`)+
-            `&optIntoOneTap=false&queryParams={}&trustedDeviceRecords={}&username=${username}`
-}
-module.exports = format
--- utils/password_manager/index.js
+++
@@ -1,9 +0,0 @@
-const encrypt = require("./encrypt")
-const format = require("./format")
-const info = require("./info")
-
-module.exports = {
-    encrypt, 
-    format,
-    info
-}
